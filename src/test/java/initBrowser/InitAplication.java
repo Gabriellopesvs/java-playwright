b80fddad19d070abadbde4e56462179fca9c0132
Gabriel Lopes <gabriellopesalmeida0@gmail.com>
2024-09-28 05:07:51 -0300

    Update file iniAplication

--- src/test/java/initBrowser/InitAplication.java
+++ src/test/java/initBrowser/InitAplication.java
@@ -6,18 +6,28 @@ import java.util.Arrays;
 
 public class InitAplication {
     Playwright playwright;
+    Browser browser;
+    BrowserContext context;
     public static Page page;
 
     public void Browser(String url) {
         playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome").setArgs(Arrays.asList("--start-fullscreen")));
-        BrowserContext context = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920, 1080));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        context = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920, 1080));
         page = context.newPage();
         page.navigate(url, new Page.NavigateOptions().setTimeout(60000));
         System.out.println(page.title());
     }
 
     public void quit() {
-        playwright.close();
+        if (context != null) {
+            context.close();
+        }
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
     }
 }
